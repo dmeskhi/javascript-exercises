a6a659b8c77f565a2f9b0f58eea2cf82cad9e1ce
David Meskhishvili <dmeskh@gmail.com>
2024-03-09 16:47:22 -0700

    Test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if(isNaN(min) || isNaN(max)) return 'ERROR';
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if(min < 0 || max < 0) return 'ERROR'
+    return (Math.abs(min-max) + 1)*(min + max)/2;
 };
 
 // Do not edit below this line
