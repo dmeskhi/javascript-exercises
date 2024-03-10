fc6b7599a7fe3eb065273770bbfbcfa89c7d9634
David Meskhishvili <dmeskh@gmail.com>
2024-03-09 17:34:37 -0700

    Test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {  
+  const farTemp = fahrenheit;
+  const farToCel = Math.round((farTemp - 32) * (5 / 9) * 10) / 10;
+  return farToCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const celTemp = celsius;
+  const celToFar = Math.round(((celTemp * 9) / 5 + 32) * 10) / 10;
+  return celToFar;
 };
 
 // Do not edit below this line
