3e913d50fe49ec4eb929829d352c98209671cdab
David Meskhishvili <dmeskh@gmail.com>
2024-03-23 22:01:21 -0700

    Palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const normalString = palindrome
+        .toLowerCase()
+        .split("")
+        .filter((character) => alphanumerical.includes(character))
+        .join();
+
+        const reversedString = normalString.split("").reverse().join("");
+        return normalString === reversedString;
 };
 
 // Do not edit below this line
